3c3158c111a01f35594d6887c77693162f5a3007
Mahletk1 <mah.kumelachew@gmail.com>
2022-10-23 01:28:14 +0300

    submitting project

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-gf74itp1.us', // the auth0 domain prefix
+    audience: 'coffe', // the audience set for the auth0 app
+    clientId: 'YfEjBfwWeOqL8m17b7PhUaM3XLy6IOtB', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
